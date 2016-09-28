630aed67bb9d12f1450accb39fcd9a4c54dd0541
Brian Romero <romerito4121@hotmail.es>
2016-09-28 11:29:57 -0500

    cambio 01

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import {
+    AfterViewInit,
+    ElementRef,
+    Component
+} from '@angular/core';
+@Component({
+  selector: 'app',
+  template: `<main></main>
+  <router-outlet></router-outlet>`
+})
+export class App {
+}
